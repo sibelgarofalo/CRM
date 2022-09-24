3f70c3395891edc0ffd7530ccb4c8fdfa99bd84b
Sibel <sblgrfl@gmail.com>
2022-09-24 11:46:53 +0200

    Initial Setup

---
+++ src/django/api/crm/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    # string version
+    def __str__(self):
+        return self.first_name
