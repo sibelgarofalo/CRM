6194cf641ae8fdce0b68a970bbe3927f8b3a152d
Sibel <sblgrfl@gmail.com>
2022-09-30 10:13:55 +0200

    Sider with behaviour

--- src/crmweb/src/components/header/crm-header.jsx
+++ src/crmweb/src/components/header/crm-header.jsx
@@ -1,12 +1,46 @@
-import { Layout } from 'antd';
+import { Layout, Avatar, Dropdown, Menu, Space, Button } from 'antd';
+import { DownOutlined, SettingOutlined, LogoutOutlined, SearchOutlined } from '@ant-design/icons';
 
 import './crm-header.less';
+
+const menu = (
+    <Menu
+        items={[
+            {
+                label: 'Logout',
+                key: '1',
+                icon: <LogoutOutlined />
+            },
+            {
+                type: 'divider',
+            },
+            {
+                label: 'Settings',
+                key: '3',
+                icon: <SettingOutlined />
+            },
+        ]}
+    />
+);
 
 const CrmHeader = (props) => {
     return (
         <div className="crm-header">
-            <Layout.Header>
-                Header
+            <Layout.Header
+                className='crm-header-content'>
+                <div className='logo' />
+                <div className='commands'>
+                    <Button shape="circle" icon={<SearchOutlined />} />
+                    <Avatar src="https://joeschmoe.io/api/v1/random" />
+                    <Dropdown overlay={menu} trigger={['click']}>
+                        <a onClick={e => e.preventDefault()}>
+                            <Space>
+                                Sibel Garofalo
+                                <DownOutlined />
+                            </Space>
+                        </a>
+                    </Dropdown>
+                </div>
             </Layout.Header>
         </div>
     )
--- src/crmweb/src/components/sider/crm-sider.jsx
+++ src/crmweb/src/components/sider/crm-sider.jsx
@@ -1,11 +1,102 @@
-import { Layout } from 'antd';
+import { useState } from 'react';
+import { Layout, Menu } from 'antd';
+import {
+    DashboardOutlined, CustomerServiceOutlined, SearchOutlined, FileAddOutlined,
+    DollarOutlined, OrderedListOutlined,
+    MenuFoldOutlined, LeftOutlined,
+    MenuUnfoldOutlined, RightOutlined
+} from '@ant-design/icons';
 
 import './crm-sider.less';
 
+const items = [
+    {
+        key: 'dashboard',
+        label: 'Dashboard',
+        icon: <DashboardOutlined />,
+        children: [
+            {
+                key: 'dashboard-sales',
+                label: 'Sales Overview',
+                icon: <DollarOutlined />,
+            },
+            {
+                key: 'dashboard-orders',
+                label: 'Orders Overview',
+                icon: <OrderedListOutlined />,
+            }
+        ]
+    },
+    {
+        key: 'customer',
+        label: 'Customers',
+        icon: <CustomerServiceOutlined />,
+        children: [
+            {
+                key: 'customer-find',
+                label: 'Find Customers',
+                icon: <SearchOutlined />,
+            },
+            {
+                key: 'customer-new',
+                label: 'New Customer',
+                icon: <FileAddOutlined />,
+            }
+        ]
+    },
+    {
+        key: 'order',
+        label: 'Orders',
+        icon: <OrderedListOutlined />,
+        children: [
+            {
+                key: 'order-find',
+                label: 'Find Orders',
+                icon: <SearchOutlined />,
+            },
+            {
+                key: 'order-new',
+                label: 'New Order',
+                icon: <FileAddOutlined />,
+            }
+        ]
+    }
+]
+
 const CrmSider = (props) => {
+
+    const [collapsed, setCollapsed] = useState(false);
+
+    const onClick = (args) => {
+        console.log(args);
+    }
+
     return (
         <div className='crm-sider'>
-            <Layout.Sider>
+            <Layout.Sider
+                theme='light'
+                collapsed={collapsed}
+                collapsible
+                trigger={null}>
+                <Menu
+                    onClick={onClick}
+                    defaultSelectedKeys={['dashboard-sales']}
+                    defaultOpenKeys={['dashboard']}
+                    mode="inline"
+                    items={items}
+                />
+                {collapsed &&
+                    <RightOutlined
+                        style={{ fontSize: 16 }}
+                        className='folder-menu'
+                        onClick={() => setCollapsed(false)} />
+                }
+                {!collapsed &&
+                    <LeftOutlined
+                        style={{ fontSize: 16 }}
+                        className='folder-menu'
+                        onClick={() => setCollapsed(true)} />
+                }
             </Layout.Sider>
         </div>
     )
